a6e80096a722aeddc0a5ecb5f6f7f6627e01f322
xandor19 <x4ndor19@gmail.com>
2022-02-01 21:21:37 -0500

    Added more function implementations

--- src/logics/Robot.scala
+++ src/logics/Robot.scala
@@ -2,10 +2,9 @@ package logics
 
 class Robot (initial: Coordinate, val steps: Array[Int]) {
   private var stepCount = 0
-  private var nextToPath = 1
+  private var nextToPath = 0
   private var current = initial
   val path = new Array[Coordinate](steps.length)
-  path(0) = initial
 
   def totalSteps: Int = stepCount
 
@@ -15,7 +14,7 @@ class Robot (initial: Coordinate, val steps: Array[Int]) {
     val destination = board.stepFrom(current, steps(stepCount))
 
     if (!destination.isSame(current)) {
-      path(nextToPath) = destination
+      path(nextToPath) = current
       current = destination
       nextToPath += 1
     }
--- src/logics/Walk.scala
+++ src/logics/Walk.scala
@@ -1,6 +1,7 @@
 package logics
 
 import scala.collection.mutable
+import scala.util.Random
 
 class Walk (val start: Coordinate, val board: Board) {
   private val family = new Array[Robot](10)
@@ -9,23 +10,42 @@ class Walk (val start: Coordinate, val board: Board) {
   private val rounds = new Array[RoundData](50)
   private var currentRound = 0
 
-  def round: Unit = {
+  def createFamily(): Unit = {
+    for (i <- 0 until 10) {
+      family(i) = new Robot(start, generateSteps())
+    }
+  }
+
+  def round(): Unit = {
+    
+  }
+
+  def sortFitness(): Unit = {
 
   }
 
-  def sortFitness: Unit = {
+  def roundStatistics: RoundData = {
 
   }
 
-  def roundStatistics: Unit = {
+  def generateSteps(): Array[Int] = {
+    val moves = Array(Step.left, Step.right, Step.up, Step.down)
+    val stepsAmount: Int = (board.width * board.depth) / 2
+    val steps = new Array[Int](stepsAmount)
 
-  }
-
-  def generateSteps: Array[Int] = {
-
+    for (i <- 0 until stepsAmount) {
+      steps(i) = moves(Random.between(0, 4))
+    }
+    steps
   }
 
   def mergeSteps(list1: Array[Int], list2: Array[Int]): Array[Int] = {
+    val l = list1.length
+    val newList = new Array[Int](l)
 
+    for (i <- 0 until l) {
+      newList(i) = if (Random.nextBoolean()) list1(i) else list2(i)
+    }
+    newList
   }
 }
